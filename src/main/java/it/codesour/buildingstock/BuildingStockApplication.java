959f1eae9e912188f54e726846848868c5bf2956
Luca Piazza <lucapiazza@MacBook-Pro-di-Luca.local>
2021-07-27 16:21:18 +0200

    initial commit

---
+++ src/main/java/it/codesour/buildingstock/BuildingStockApplication.java
@@ -0,0 +1,13 @@
+package it.codesour.buildingstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/codesour/buildingstock/BuildingStockApplicationTests.java
@@ -0,0 +1,13 @@
+package it.codesour.buildingstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
